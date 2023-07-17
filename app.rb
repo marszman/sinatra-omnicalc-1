da69d540b4b42ad8e59b5894f2ae215790389807
marszman <136658905+marszman@users.noreply.github.com>
2023-07-17 11:05:09 +0000

    omnicalc1 - initial framework

--- app.rb
+++ app.rb
@@ -2,8 +2,37 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:landing)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:squareroot_new)
+end
+
+get("/square_root/results") do
+  erb(:squareroot_results)
+end
+
+get("/payment/new") do
+  erb(:payment_new)
+end
+
+get("/payment/results") do
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random_new)
+end
+
+get("/random/results") do
+  erb(:random_results)
 end
